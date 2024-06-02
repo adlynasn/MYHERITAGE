7cf07aacd959dee5e29438308822d609c27bf564
adlynasn <nuradlina230302@gmail.com>
2024-06-02 23:05:41 +0800

    merge main

---
+++ routes/categories.js
@@ -0,0 +1,24 @@
+const{Category}=require('MYHERITAGE\resources\js\category.js');
+const express=require('express');
+const router=express.Router();
+
+router.get('/',async(req,res)=>{
+    const categoryList=await Category.find();
+
+    if(!categoryList){
+        res.status(500).json({success:false})
+    }
+    res.send(categoryList);
+})
+
+
+router.post('/',async(req,res)=>{
+    let category=new Category({
+        name:req.body.name,
+        icon:req.body.icon,
+        color:req.body.color
+    })
+    
+})
+
+module.exports=router;
